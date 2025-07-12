26d27fecd6f8575ca84611d1b43ee2fbafc0dfff
Daniel Benard <skidan007@gmail.com>
2025-07-12 11:20:45 +0100

    userprofile added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
 import MainContent from "./components/MainContent";
@@ -35,6 +36,7 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   );
