f72db7dae18ae344187f7e8d8d98421886452947
Daniel Victor <skidan007@gmail.com>
2025-07-19 17:20:35 +0100

    style added

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer>
+    <footer style={{backgroundColor: 'black', color: 'white'}}> 
       <p>© 2023 City Lovers</p>
     </footer>
   );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main>
+    <main style={{backgroundColor: 'black', color: 'white'}}>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
