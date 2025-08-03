a36501efd78de5696170872706ec63e94744c23b
Daniel Benard <skidan007@gmail.com>
2025-08-03 20:09:09 +0100

    API setup

---
+++ github-user-search/github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center text-blue-600">
+        GitHub User Search
+      </h1>
+      {/* Future Components: <SearchBar />, <UserCard /> */}
+    </div>
+  );
+}
+
+export default App;
