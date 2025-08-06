aa51feb913fe98f52573b9009e883e73f8c4c996
Daniel Benard <skidan007@gmail.com>
2025-08-06 06:49:03 +0100

    styling component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
         alt="User" className="rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600">
+      <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
