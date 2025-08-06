da91358fe1ea886fb0b96a455e30d03cf1b7f447
Daniel Benard <skidan007@gmail.com>
2025-08-06 07:09:18 +0100

    responsive

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 p-8 xs:max-w-xs max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
-        alt="User" className="rounded-full w-36 h-36 mx-auto"
+        alt="User" className="rounded-full sm:w-24 sm:h-24 w-36 h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="sm:text-lg text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
