9f9c181483c92fe37bc48c3b4a51cd82d2c7b972
Daniel Benard <skidan007@gmail.com>
2025-08-12 21:47:37 +0100

    Home page recipe added

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
         Recipe Sharing Platform
       </h1>
 
-      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2">
+      <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
